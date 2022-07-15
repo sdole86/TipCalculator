2ccd3bdea3dadf247f83f560188732a1a2446725
sdole86 <92174592+sdole86@users.noreply.github.com>
2022-07-14 23:17:44 -0400

    Initial commit

---
+++ TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+using System;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Cost = 0;
+            ViewBag.FifteenPerTip = 0;
+            ViewBag.TwentyPerTip = 0;
+            ViewBag.TwentyFivePerTip = 0;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator calculator)
+        {
+
+                if (ModelState.IsValid)
+                {
+                    ViewBag.FifteenPerTip = calculator.GetTip(15);
+                    ViewBag.TwentyPerTip = calculator.GetTip(20);
+                    ViewBag.TwentyFivePerTip = calculator.GetTip(25);
+                }
+                else
+                {
+                    ViewBag.FifteenPerTip = 0;
+                    ViewBag.TwentyPerTip = 0;
+                    ViewBag.TwentyFivePerTip = 0;
+                }
+                return View(calculator);
+        }
+    }
+}
---
+++ TipCalculator/Models/TipCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage = "Please enter a cost.")]
+        [Range(.01, double.PositiveInfinity, ErrorMessage = "Cost amount must be greater than 0")]
+
+        public decimal? Cost { get; set; }
+        public decimal? FifteenPerTip { get; set; }
+        public decimal? TwentyPerTip { get; set; }
+        public decimal? TwentyFivePerTip { get; set; }
+
+        public decimal? GetTip(decimal tip)
+        {
+            decimal? TotalTip = this.Cost * (tip / 100);
+            return TotalTip;
+        }
+
+    }
+}
